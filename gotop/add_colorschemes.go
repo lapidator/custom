ff7eb919679445a7a1dfca613054890f2f0bd3b3
localuser <localuser@worf.ph1.uni-koeln.de>
2022-12-16 17:54:58 +0100

    initial commit for gotop customization (here: colorscheme)

---
+++ gotop/add_colorschemes.go
@@ -0,0 +1,8 @@
+// This file is necessary to initialize/register color schemes,
+// which are specified in their own files `{name}.json`,
+// where {name} is the color scheme's name that needs to be listed here.
+package colorschemes
+
+func init() {
+	register("own")
+}
